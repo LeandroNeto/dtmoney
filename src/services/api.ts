1ac1879d1d1ae631f177c3e8b3bbb948e501a961
LeandroNeto <leandrohock@gmail.com>
2022-03-28 20:13:57 -0300

    Incluída a informação da Url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
+  /* Caso queira testar localmente, apenas remover o comentário a url do localhost
+   e comentar a linha da url na web */
   // baseURL: 'http://localhost:3000/api',
-  baseURL: 'https://dtmoney-leandroneto.vercel.app/api',
+  baseURL: 'https://dtmoney-beta-ten.vercel.app/api',
 })
